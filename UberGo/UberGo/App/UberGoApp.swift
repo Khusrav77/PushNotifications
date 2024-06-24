cbeffd810717bfad7bffe4d31d4e14775d100a72
Khusrav Safiev <147575346+Khusrav77@users.noreply.github.com>
2024-06-25 00:06:32 +0300

    HomeView

--- UberGo/UberGo/App/UberGoApp.swift
+++ UberGo/UberGo/App/UberGoApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct UberGoApp: App {
     var body: some Scene {
         WindowGroup {
-            
+            HomeView()
         }
     }
 }
---
+++ UberGo/UberGo/Core/Views/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  UberGo
+//
+//  Created by Khusrav Safiev on 6/25/24.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
